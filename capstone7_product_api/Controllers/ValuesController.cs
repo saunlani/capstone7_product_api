2db237638da9be1f3a5a693c936d511a68526975
saunlani <saunlani@gmail.com>
2018-06-09 23:14:37 -0400

    added category and supplier ID API search functionality

--- capstone7_product_api/Controllers/ValuesController.cs
+++ capstone7_product_api/Controllers/ValuesController.cs
@@ -43,7 +43,7 @@ namespace capstone7_product_api.Controllers
         {
             NORTHWNDEntities db = new NORTHWNDEntities();
             List<Product> Products = db.Products.ToList();
-            List <ProductShort>IDAndNames = Products.Select(t => new ProductShort { ProductID = t.ProductID, ProductName = t.ProductName }).ToList();
+            List <ProductShort>IDAndNames = Products.Select(p => new ProductShort { ProductID = p.ProductID, ProductName = p.ProductName }).ToList();
             return IDAndNames; 
         }
 
@@ -53,15 +53,75 @@ namespace capstone7_product_api.Controllers
         {
             NORTHWNDEntities db = new NORTHWNDEntities();
             List<Product> Products = db.Products.ToList();
-
-            Product Result = (from m in db.Products
-                           where m.ProductID == id
-                           select m).Single();
+            Product Result = null;
+            try
+            {
+                Result = (from m in db.Products
+                          where m.ProductID == id
+                          select m).Single();
+            }
+            catch
+            {
+                Exception e;
+            }
             return Result;
-
-            //List<ProductShort> IDAndNames = Products.Select(t => new ProductShort { ProductID = t.ProductID, ProductName = t.ProductName }).ToList();
-            //return IDAndNames;
         }
+
+        [HttpGet]
+        [Route("{api}/Category")]
+        public List<Product> Category(int id)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            List<Product> Products = db.Products.ToList();
+            List<Product> CatIDList = (from p in db.Products
+                                       where p.CategoryID == id
+                                       select p).ToList();
+            return CatIDList;
+        }
+
+        [HttpGet]
+        [Route("{api}/Category")]
+        public List<Product> Category()
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            List<Product> Products = db.Products.ToList();
+            return Products;
+        }
+
+        [Route("{api}/Supplier")]
+        public List<Product> Supplier(int id)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            List<Product> Products = db.Products.ToList();
+            List<Product> SupplierIDList = (from p in db.Products
+                                       where p.SupplierID == id
+                                       select p).ToList();
+            return SupplierIDList;
+        }
+
+        [HttpGet]
+        [Route("{api}/Supplier")]
+        public List<Product> Supplier()
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            List<Product> Products = db.Products.ToList();
+            return Products;
+        }
+        //NORTHWNDEntities db = new NORTHWNDEntities();
+        //List<Product> Products = db.Products.ToList();
+        //Product Result = null;
+        //try
+        //{
+        //    Result = (from m in db.Products
+        //              where m.CategoryID == id
+        //              select m).Single();
+
+        //}
+        //catch
+        //{
+        //    Exception e;
+        //}
+        //return Result;
 
         //return Json(db.Products.Select(t => new { Id = t.ProductID, Name = t.ProductName }).ToList());
         //if (id == null)
@@ -75,5 +135,8 @@ namespace capstone7_product_api.Controllers
         //    return NWProducts;
 
         //
+
+        //List<ProductShort> IDAndNames = Products.Select(t => new ProductShort { ProductID = t.ProductID, ProductName = t.ProductName }).ToList();
+        //return IDAndNames;
     }
 }
